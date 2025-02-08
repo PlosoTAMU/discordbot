286b4f7fde05464dadd008635f3087f01cd2992b
PlosoTAMU <truman.porter@tamu.edu>
2025-02-08 15:17:28 -0600

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,8 +1,67 @@
+import datetime
+
 import discord
 from discord.ext import commands, tasks
 import os
-from datetime import datetime, time, timedelta
+from dotenv import load_dotenv
+import openmeteo_requests
+import pandas as pd
+from retry_requests import retry
+import requests_cache
 
+
+# Setup the Open-Meteo API client with cache and retry on error
+cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
+retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
+openmeteo = openmeteo_requests.Client(session = retry_session)
+
+
+url = "https://api.open-meteo.com/v1/forecast"
+params = {
+	"latitude": 30.628,
+	"longitude": -96.3344,
+	"hourly": "temperature_2m",
+    "temperature_unit": "fahrenheit",
+	"timezone": "America/Chicago",
+	"forecast_days": 1,
+	"models": "best_match"
+}
+responses = openmeteo.weather_api(url, params=params)
+
+# Process first location. Add a for-loop for multiple locations or weather models
+response = responses[0]
+print(f"Coordinates {response.Latitude()}°N {response.Longitude()}°E")
+print(f"Elevation {response.Elevation()} m asl")
+print(f"Timezone {response.Timezone()} {response.TimezoneAbbreviation()}")
+print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
+
+# Process hourly data. The order of variables needs to be the same as requested.
+hourly = response.Hourly()
+hourly_temperature_2m = hourly.Variables(0).ValuesAsNumpy()
+
+hourly_data = {"date": pd.date_range(
+	start = pd.to_datetime(hourly.Time(), unit = "s", utc = True),
+	end = pd.to_datetime(hourly.TimeEnd(), unit = "s", utc = True),
+	freq = pd.Timedelta(seconds = hourly.Interval()),
+	inclusive = "left"
+)}
+
+hourly_data["temperature_2m"] = hourly_temperature_2m
+
+hourly_dataframe = pd.DataFrame(data = hourly_data)
+print(hourly_dataframe)
+
+# Extract the temperature at 7am
+temperature_at_7am = hourly_dataframe.loc[hourly_dataframe['date'].dt.hour == 12, 'temperature_2m'].values
+if temperature_at_7am.size > 0:
+    print(f"Temperature at 6am: {temperature_at_7am[0]}")
+else:
+    print("No temperature data available for 6am")
+    temperature_at_7am = "None"
+
+
+
+load_dotenv()
 
 # Get the API token from the .env file.
 TOKEN = os.getenv("TOKEN")
@@ -34,8 +93,7 @@ async def on_ready():
 async def send_message():
     channel = client.get_channel(CHANNEL)
     if channel:
-        await channel.send("whoopywhoop")
+        await channel.send(f"Temp @6am: {temperature_at_7am[0]:.2f}°F")
         await client.close()  # Exit after sending the message
 
 client.run(TOKEN)
-
