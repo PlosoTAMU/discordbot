d660b58f7296556e63b892dd8e0035945242dcf4
PlosoTAMU <truman.porter@tamu.edu>
2025-02-08 09:35:37 -0600

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,10 +1,8 @@
 import discord
 from discord.ext import commands, tasks
 import os
-from dotenv import load_dotenv
 from datetime import datetime, time, timedelta
 
-load_dotenv()
 
 # Get the API token from the .env file.
 TOKEN = os.getenv("TOKEN")
